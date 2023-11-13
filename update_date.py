f9a19db3ab3f0a59cd3fe0c5cc9fc5f3f56794a8
Emirali Caferzade <ecaferzade@hotmail.com>
2023-11-13 13:29:37 +0100

    first working draft copleted.

--- update_date.py
+++ update_date.py
@@ -1,10 +1,24 @@
-import zipfile
+from odf import text, teletype
+from odf.opendocument import load
 
+odt_file_path = '/home/emirali/Documents\
+/EmiraliCaferzade_Belgeler/Code/Projects\
+/update_date_in_cv/emirali_CV_libreOffice_template.odt'
 
-zip_file_url = '/home/emirali/Documents/\
-EmiraliCaferzade_Belgeler/Code/Projects/\
-update_date_in_cv/emirali_CV_libreOffice_template.zip'
+updated_date = 'Berlin, den 13. November 2023'
 
-with zipfile.ZipFile(zip_file_url) as my_zip:
-    with my_zip.open('emirali_CV_libreOffice_template.odt') as my_file:
-        print(my_file.read())
+text_document = load(odt_file_path)
+all_paragraps = text_document.getElementsByType(text.P)
+paragraph = all_paragraps[-4]
+extracted_date_line = teletype.extractText(paragraph)
+core_date = extracted_date_line.replace('\t', '')
+extracted_date_line = extracted_date_line.\
+    replace(core_date, updated_date)
+
+new_element = text.P()
+new_element.addText(extracted_date_line)
+
+paragraph.parentNode.insertBefore(new_element, paragraph)
+paragraph.parentNode.removeChild(paragraph)
+
+text_document.save('result.odt')
